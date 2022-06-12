a53cb0e7d792dd4092d3ce4acefc09389f9cd5a8
Aftab Sidhu <Aftab180@gmail.com>
2022-06-12 17:58:10 -0400

    commit tree a065b72caee7e86c5bec4e891e1e3d25c6b8274f

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,17 @@
 class Person
+    def name=(person_name)
+        @name = person_name
+    end
 
+    def name
+        @name 
+    end
+
+    def job=(person_name)
+        @job = person_name
+    end
+
+    def job
+        @job 
+    end
 end
